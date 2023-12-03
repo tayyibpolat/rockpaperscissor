a792d4401419dac02e7ca0a2cf2a2b2ae48be1de
Tayyib Polat <tayyibpolat@gmx.de>
2023-12-03 13:29:19 +0100

    Made getComp...  func now working

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello")
+function getComputerChoice(){
+    rand = Math.floor(Math.random() * 4);
+    if (rand > 2){
+        rand = alert("Rock");
+    }
+    else if (rand > 1){
+        rand = alert("Paper");
+    }
+    else if (rand > 0){
+        rand = alert("Scissor");
+    }
+    return rand;
+}
