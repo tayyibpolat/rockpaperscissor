fc982193605a2c720e777c7ddf378576638324e3
Tayyib Polat <tayyibpolat@gmx.de>
2023-12-09 19:22:14 +0100

    Created while do loop the game plays again if tie

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-function getComputerChoice(){
-    rand = Math.floor(Math.random() * 4);
-    if (rand > 2){
-        rand = alert("Rock");
+function getComputerChoice() {
+    const rand = Math.floor(Math.random() * 3);
+    if (rand === 0) {
+        return "Rock";
+    } else if (rand === 1) {
+        return "Paper";
+    } else {
+        return "Scissor";
     }
-    else if (rand > 1){
-        rand = alert("Paper");
-    }
-    else if (rand > 0){
-        rand = alert("Scissor");
-    }
-    return rand;
 }
+
+function game(computerSelection, playerSelection) {
+    let result;
+
+    if (
+        (playerSelection === "rock" && computerSelection === "Scissor") ||
+        (playerSelection === "paper" && computerSelection === "Rock") ||
+        (playerSelection === "scissor" && computerSelection === "Paper")
+    ) {
+        result = "You won";
+    } else if (
+        (playerSelection === "scissor" && computerSelection === "Rock") ||
+        (playerSelection === "rock" && computerSelection === "Paper") ||
+        (playerSelection === "paper" && computerSelection === "Scissor")
+    ) {
+        result = "You lost";
+    } else {
+        result = "It's a tie";
+    }
+
+    alert(result);
+    return result;
+}
+
+let result;
+do {
+    const computerChoice = getComputerChoice();
+    const playerChoice = prompt("rock, paper, or scissor?").toLowerCase();
+    result = game(computerChoice, playerChoice);
+} while (result === "It's a tie");
+
+
